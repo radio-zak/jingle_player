aafef0ef9a03c45bbb5d48ce833eac4d9c729167
Kacper Zieliński <kzielinski@omenproductions.com>
2026-05-10 21:47:29 +0200

    chore: make palette selector paginated

--- lib/ui/palette_selector.dart
+++ lib/ui/palette_selector.dart
@@ -12,38 +12,64 @@ class PaletteSelector extends StatelessWidget {
       fit: FlexFit.tight,
       child: Consumer<AudioHandler>(
         builder: (context, player, child) {
-          return CarouselView.weighted(
-            padding: EdgeInsets.all(8),
-            key: UniqueKey(),
-            itemSnapping: true,
-            flexWeights: [1, 1, 1, 1],
-            shrinkExtent: 64,
-            controller: CarouselController(initialItem: 1),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusGeometry.circular(8),
-            ),
-            scrollDirection: Axis.horizontal,
-            children: List.generate(player.palettes, (int index) {
-              final buttonLabel = (index + 1).toString();
-              return ActionButton(
-                label: buttonLabel,
-                hoverColor: player.editMode
-                    ? Colors.orangeAccent
-                    : Colors.tealAccent,
-                onPressed: player.editMode
-                    ? () async {
-                        await player.savePalette(index);
-                      }
-                    : () async {
-                        await player.getPalette(index);
-                      },
-                color: player.editMode
-                    ? Colors.orange
-                    : player.activePalette == index
-                    ? Theme.of(context).colorScheme.primary
-                    : Theme.of(context).colorScheme.primaryFixedDim,
-              );
-            }),
+          final carouselController = CarouselController(
+            initialItem: (player.activePalette < 4) ? 0 : 4,
+          );
+          return Flex(
+            direction: Axis.horizontal,
+            mainAxisSize: MainAxisSize.max,
+            spacing: 2,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back_sharp),
+                onPressed: () {
+                  carouselController.animateToItem(0);
+                },
+              ),
+              Expanded(
+                child: CarouselView.weighted(
+                  padding: EdgeInsets.all(8),
+                  key: UniqueKey(),
+                  itemSnapping: true,
+                  flexWeights: [1, 1, 1, 1],
+                  shrinkExtent: 64,
+                  controller: carouselController,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(8),
+                  ),
+                  enableSplash: false, // makes children clickable
+                  scrollDirection: Axis.horizontal,
+                  children: List.generate(player.palettes, (int index) {
+                    final buttonLabel = (index + 1).toString();
+                    return ActionButton(
+                      label: buttonLabel,
+                      hoverColor: player.editMode
+                          ? Colors.orangeAccent
+                          : Colors.tealAccent,
+                      onPressed: player.editMode
+                          ? () async {
+                              await player.savePalette(index);
+                            }
+                          : () async {
+                              await player.getPalette(index);
+                            },
+                      color: player.editMode
+                          ? Colors.orange
+                          : player.activePalette == index
+                          ? Theme.of(context).colorScheme.primary
+                          : Theme.of(context).colorScheme.primaryFixedDim,
+                    );
+                  }),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward_sharp),
+                onPressed: () {
+                  carouselController.animateToItem(4);
+                },
+              ),
+            ],
           );
         },
       ),
